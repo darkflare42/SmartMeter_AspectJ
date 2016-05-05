860424c6cefd11b670d242dbd2f2d9f77d1ae9e3
Or Keren(Home) <or.keren1@mail.huji.ac.il>
2016-05-05 17:30:07 +0300

    Added some GUI forms

---
+++ src/DBComm.java
@@ -0,0 +1,39 @@
+/**
+ * Created by Or Keren on 05/05/2016.
+ */
+public class DBComm {
+
+    public static void addNewMeter(PowerMeter newMeter){
+
+    }
+
+    public static void addNewCustomer(Customer newCustomer){
+
+    }
+
+    public static void addMeterToCustomer(PowerMeter meter, Customer customer){
+
+    }
+
+    public static void removeMeterFromCustomer(PowerMeter meter, Customer customer){
+
+    }
+
+    public static void deleteCustomer(Customer customer){
+
+    }
+
+    public static void deltePowerMeter(PowerMeter meter){
+
+    }
+
+    public static void getAllMeters(){
+
+    }
+
+    public static void getAllCustomers(){
+
+    }
+
+
+}
---
+++ src/MeterCommunication.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Or Keren on 05/05/2016.
+ */
+public class MeterCommunication {
+
+    public static int getCurrentReading(int meterID){
+
+    }
+
+    public static int getTotalReading(int meterID){
+
+    }
+}
