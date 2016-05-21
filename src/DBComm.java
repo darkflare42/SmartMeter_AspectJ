1999bd6768649431931924194353fa78c0174932
Omer <omer.ornan@mail.huji.ac.il>
2016-05-21 13:52:57 +0300

    add date to login screen, now everything else will be added

--- src/DBComm.java
+++ src/DBComm.java
@@ -1,7 +1,5 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
+import java.util.*;
 
 /**
  * Created by Or Keren on 05/05/2016.
@@ -71,6 +69,109 @@ public class DBComm {
     public static User getUser(String username){
         return null;
     }
+
+
+
+
+    public static void insertDataLogIn(int userID, String userName, String password) {
+
+        Connection conn = null;
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost/smartgrid?" +
+                            "user=root&password=root");
+            String query = " insert into login (user_id, user_name, password)"
+                    + " values (?, ?, ?)";
+
+            // create the mysql insert preparedstatement
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setInt(1, userID);
+            preparedStmt.setString(2, userName);
+            preparedStmt.setString(3, password);
+            // execute the preparedstatement
+            preparedStmt.execute();
+            // Do something with the Connection
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+    public static ResultSet getDataFromLoginByUserId(int userId) {
+        Connection conn = null;
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost/smartgrid?" +
+                            "user=root&password=root");
+            insertDataLogIn(324234, "Dgdg", "dfg");
+            Statement st = conn.createStatement();
+            ResultSet d = st.executeQuery("SELECT * FROM  login WHERE user_id=" + userId);
+            // Do something with the Connection
+            return d;
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendoreError: " + ex.getErrorCode());
+        }
+
+        return null;
+    }
+
+
+
+    public static void insertDataBill(int userId, int currentBill, java.util.Date lastDateToPay, boolean payedBool) {
+        Connection conn = null;
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost/smartgrid?" +
+                            "user=root&password=root");
+            String query = " insert into bill (user_id, current_bill, last_date_to_pay, payed)"
+                    + " values (?, ?, ?,?)";
+
+            // create the mysql insert preparedstatement
+            Object timestamp = new java.sql.Timestamp(lastDateToPay.getTime());
+
+            PreparedStatement preparedStmt = conn.prepareStatement(query);
+            preparedStmt.setInt(1, userId);
+            preparedStmt.setInt(2, currentBill);
+            preparedStmt.setObject(3, timestamp);
+            preparedStmt.setBoolean(4, payedBool);
+            // execute the preparedstatement
+            preparedStmt.execute();
+            // Do something with the Connection
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
+
+
+    public static ResultSet getDataFromBillByUserId(int userId) {
+        Connection conn = null;
+        try {
+            conn =
+                    DriverManager.getConnection("jdbc:mysql://localhost/smartgrid?" +
+                            "user=root&password=root");
+            insertDataLogIn(324234, "Dgdg", "dfg");
+            Statement st = conn.createStatement();
+            ResultSet d = st.executeQuery("SELECT * FROM  bill WHERE user_id=" + userId);
+            // Do something with the Connection
+            return d;
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendoreError: " + ex.getErrorCode());
+        }
+
+        return null;
+    }
+
 
 
 }
--- src/MeterCommunication.java
+++ src/MeterCommunication.java
@@ -4,10 +4,11 @@
 public class MeterCommunication {
 
     public static int getCurrentReading(int meterID){
+        return 1;
 
     }
 
     public static int getTotalReading(int meterID){
-
+        return  1;
     }
 }
